11a6f52b4af9e7b955e41cee1263119a65fba136
Sourena Yadegari <NN>
2023-07-24 22:31:47 +0200

    more tests for models

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,15 +1,25 @@
 import unittest
-from dqn.model import QNetwork
+from dqn.model import QNetwork, DuelingQNetwork
 
 import torch
 
 class TestQNetwork(unittest.TestCase):
-    def test_forward(self):
+    def test_QNetwork_forward(self):
         state_size = 8
         action_size = 4
         seed = 101
-        m = QNetwork(state_size, action_size, seed)
-        assert m.forward(torch.rand(state_size)).shape[0] == action_size
+        m = QNetwork(seed, state_size, action_size)
+        out = m.forward(torch.rand(1, state_size))
+        assert out.shape[1] == action_size
+
+    def test_DuelingQNetwork_forward(self):
+        state_size = 8
+        action_size = 4
+        seed = 101
+        m = DuelingQNetwork(seed, state_size, action_size)
+        out = m.forward(torch.rand(1, state_size))
+        assert out.shape[1] == action_size
+
 
 if __name__ == '__main__':
     unittest.main()
